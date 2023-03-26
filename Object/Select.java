06736a2688580ee8ff37d7d44843e43e3d67bc71
hao0131 <hao01312000@gmail.com>
2023-03-27 02:00:03 +0800

    fix 選取框bug
    選取框只能往右下角畫

--- Object/Select.java
+++ Object/Select.java
@@ -1,38 +1,51 @@
 package Object;
 
 import java.awt.*;
-import java.util.ArrayList;
 
 public class Select {
-
-    private Point startPoint;
-    private Point endPoint;
+    private int startX;
+    private int startY;
+    private int endX;
+    private int endY;
     private Rectangle rect;
 
     public Select(Point startPoint, Point endPoint){
-        this.startPoint = startPoint;
-        this.endPoint = endPoint;
-        rect = new Rectangle((int)startPoint.getX(), (int)startPoint.getY(), (int)(endPoint.getX() - startPoint.getX()), (int)(endPoint.getY() - startPoint.getY()));
+        startX = (int)startPoint.getX();
+        startY = (int)startPoint.getY();
+        endX = (int)endPoint.getX();
+        endY = (int)endPoint.getY();
+
+        rect = new Rectangle(startX, startY, endX - startX, endY - startY);
     }
 
     public void updateEndPoint(Point endPoint){
-        this.endPoint = endPoint;
-        rect.setRect((int)startPoint.getX(), (int)startPoint.getY(), (int)(endPoint.getX() - startPoint.getX()), (int)(endPoint.getY() - startPoint.getY()));
+        int x = (int) endPoint.getX();
+        int y = (int) endPoint.getY();
+        
+        if (x < startX && y < startY) { // 左上
+            rect.setRect(x, y, startX - x, startY - y);
+        } else if (x < startX && y >= startY) { // 左下
+            rect.setRect(x, startY, startX - x, y - startY);
+        } else if (x >= startX && y < startY) { // 右上
+            rect.setRect(startX, y, x - startX, startY - y);
+        } else { // 右下
+            rect.setRect(startX, startY, x - startX, y - startY);
+        }
     }
 
     public void drawArea(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
         
         g2d.setStroke(new BasicStroke(2));
-        g.setColor(Color.BLACK);
-        g.drawRect((int)startPoint.getX(), (int)startPoint.getY(), (int)(endPoint.getX() - startPoint.getX()), (int)(endPoint.getY() - startPoint.getY()));    
+        g.setColor(Color.BLACK); 
+        g.drawRect((int) rect.getX(), (int) rect.getY(), (int) rect.getWidth(), (int) rect.getHeight());
 
         AlphaComposite alpha = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
         g2d.setComposite(alpha);
 
         g2d.setStroke(new BasicStroke());
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect((int)startPoint.getX(), (int)startPoint.getY(), (int)(endPoint.getX() - startPoint.getX()), (int)(endPoint.getY() - startPoint.getY()));
+        g.fillRect((int) rect.getX(), (int) rect.getY(), (int) rect.getWidth(), (int) rect.getHeight());
     }
 
     public boolean isContain(BasicObject basicObject){
